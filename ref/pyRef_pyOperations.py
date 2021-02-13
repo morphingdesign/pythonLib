4c113c66914b6402f9236d42c545390c018912ff
Hans <architect.hp@gmail.com>
2021-02-12 21:57:12 -0600

    Add starting file for general operations in Python, namely the use of unittest for function checks.

---
+++ ref/pyRef_pyOperations.py
@@ -0,0 +1,37 @@
+# -----------------------------------------------------------
+# pyRef_pyOperations.py
+# v.1.0
+# Updated: 20210212
+# -----------------------------------------------------------
+
+"""
+Outline of general operations in Python.
+"""
+
+# -----------------------------------------------------------
+# FUNCTION OPERATIONS ))))))))))))))))))))))))))))))))) START
+# -----------------------------------------------------------
+
+# __name__ Check
+# Use __name__ check to run function within current file, while
+# also allow use of the function to be imported into other files
+# for use outside of this file.
+
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
+
+# True: Runs the function within the context of this file.
+# False: Function is being run via another file and therefore
+#   out of the context of this file.
+
+# Source: https://www.freecodecamp.org/news/whats-in-a-python-s-name-506262fe61e8/
+
+#************************************************************
+
+
+
+# -----------------------------------------------------------
+# FUNCTION OPERATIONS ))))))))))))))))))))))))))))))))))) END
+# -----------------------------------------------------------
