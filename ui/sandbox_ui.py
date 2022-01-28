75abd1ca4dfc9dafeb570ca3208c0df58d50971a
Hans Palacios <architect.hp@gmail.com>
2022-01-27 22:12:09 -0600

    Align frozen column with base table view.

--- ui/sandbox_ui.py
+++ ui/sandbox_ui.py
@@ -10,26 +10,48 @@ class Root(QtWidgets.QMainWindow):
 
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
-        # self.main = Widget()
+        self._main_defaults()
+        self.main = QtWidgets.QWidget()
+        self.setCentralWidget(self.main)
+        self._h_layout = QtWidgets.QHBoxLayout()
+        self._v_layout = QtWidgets.QVBoxLayout()
+        self.main.setLayout(self._v_layout)
+        self._v_layout.addLayout(self._h_layout)
+        self._v_layout.setAlignment(QtCore.Qt.AlignTop)
+        self.btn = QtWidgets.QPushButton('test')
+        self._h_layout.addWidget(self.btn)
 
         # Setup model and data.
         self.model = QtGui.QStandardItemModel()
-        self.header_labels = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8']
-        self.num_of_cols = len(self.header_labels)
-        self.num_of_rows = 14
+        # self.header_labels = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8']
+        self.num_of_cols = 10
+        # Generate header labels via list comprehension.
+        self.header_labels = [('Column %d' % (i + 1)) for i in range(self.num_of_cols)]
+        self.num_of_rows = 35
 
         self.model.setHorizontalHeaderLabels(self.header_labels)
         self.model.setColumnCount(self.num_of_cols)
         self.model.setRowCount(self.num_of_rows)
-
-
         self.data = Data(self.num_of_cols, self.num_of_rows)
         self._populate_data()
 
-        self.main = Table(self.model)
-        self.main.setMinimumSize(600, 500)
-        # self.main.resize(500, 600)
-        self.main.show()
+        self.main_table = Table(self.model)
+        self.main_table.setMinimumSize(600, 500)
+        self._v_layout.addWidget(self.main_table)
+
+        self.show()
+
+    def _main_defaults(self):
+        """"""
+        self.setWindowTitle('Sandbox')
+        menu = self.menuBar()
+        file_menu = menu.addMenu('File')
+
+        status = self.statusBar()
+        status.showMessage('Status Bar')
+
+        self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.setMaximumHeight(500)
 
     def _populate_data(self):
         """"""
@@ -39,8 +61,6 @@ class Root(QtWidgets.QMainWindow):
                 cell_datum = QtGui.QStandardItem(str(col_data))
                 cell_datum.setTextAlignment(QtCore.Qt.AlignCenter)
                 self.model.setItem(row_num, col_num, cell_datum)
-
-
 
 
 # class Widget(QWidget):
@@ -91,7 +111,8 @@ class Root(QtWidgets.QMainWindow):
 class Data:
     def __init__(self, columns, rows):
         # Create matrix by columns.
-        self.data = np.zeros((columns, rows))
+        # self.data = np.zeros((columns, rows))
+        self.data = np.random.randint(100, size=(columns, rows))
         # Convert numpy array to nested list.
         self.data_list = self.data.tolist()
         # Transpose matrix from columns to rows.
@@ -109,12 +130,22 @@ class Table(QtWidgets.QTableView):
         self.setModel(model)
         self.frozen_table = QtWidgets.QTableView(self)
         self.init_frozen_table()
+        #
+        self.horizontalHeader().sectionResized.connect(self.updateSectionWidth)
+        self.verticalHeader().sectionResized.connect(self.updateSectionHeight)
+        self.frozen_table.verticalScrollBar().valueChanged.connect(
+            self.verticalScrollBar().setValue)
+        self.verticalScrollBar().valueChanged.connect(
+            self.frozen_table.verticalScrollBar().setValue)
 
     def init_frozen_table(self):
         # Assign model to be the same as the other table's model.
         self.frozen_table.setModel(self.model())
         # Hide frozen table's vertical labels.
         self.frozen_table.verticalHeader().hide()
+        #
+        self.frozen_table.horizontalHeader().setSectionResizeMode(
+            QtWidgets.QHeaderView.Fixed)
         # Position main table under frozen table.
         self.viewport().stackUnder(self.frozen_table)
         # Style frozen table to appear unique from main table.
@@ -123,6 +154,10 @@ class Table(QtWidgets.QTableView):
                                  background-color: #8EDE21;
                                  selection-background-color: #999;
                     }''')
+        #
+        self.frozen_table.setFocusPolicy(QtCore.Qt.NoFocus)
+        #
+        self.frozen_table.setSelectionModel(self.selectionModel())
         # Setup columns and isolate first column only.
         for col in range(1, self.model().columnCount()):
             self.frozen_table.setColumnHidden(col, True)
@@ -138,6 +173,35 @@ class Table(QtWidgets.QTableView):
         self.setHorizontalScrollMode(self.ScrollPerPixel)
         self.setVerticalScrollMode(self.ScrollPerPixel)
         self.frozen_table.setVerticalScrollMode(self.ScrollPerPixel)
+
+    def updateSectionWidth(self, logicalIndex, oldSize, newSize):
+        if self.logicalIndex == 0:
+            self.frozen_table.setColumnWidth(0, newSize)
+            self.update_frozen_table_geometry()
+
+    def updateSectionHeight(self, logicalIndex, oldSize, newSize):
+        self.frozen_table.setRowHeight(logicalIndex, newSize)
+
+    def resizeEvent(self, event):
+        """Resize frozen table alongside main window/table resize."""
+        super(Table, self).resizeEvent(event)
+        self.update_frozen_table_geometry()
+
+    def moveCursor(self, cursorAction, modifiers):
+        current = super(Table, self).moveCursor(cursorAction, modifiers)
+        if (cursorAction == self.MoveLeft and
+                self.current.column() > 0 and
+                self.visualRect(current).topLeft().x() <
+                    self.frozen_table.columnWidth(0)):
+            newValue = (self.horizontalScrollBar().value() +
+                        self.visualRect(current).topLeft().x() -
+                        self.frozen_table.columnWidth(0))
+            self.horizontalScrollBar().setValue(newValue)
+        return current
+
+    def scrollTo(self, index, hint):
+        if index.column() > 0:
+            super(Table, self).scrollTo(index, hint)
 
     def update_frozen_table_geometry(self):
         self.frozen_table.setGeometry(
